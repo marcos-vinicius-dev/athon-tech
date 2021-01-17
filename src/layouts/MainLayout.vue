f4b3325b109486d40c8fbfc27382004ff19d370a
marcos vinicius <marcvinicius.1997@gmail.com>
2021-01-17 01:00:28 -0300

    feat: layout principal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,107 +1,48 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
-</template>
-
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import { mdiMenu } from '@quasar/extras/mdi-v5'
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
+
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      mdiMenu: ''
     }
+  },
+
+  created () {
+    this.mdiMenu = mdiMenu
   }
 }
 </script>
+
+<template lang="pug">
+q-layout(view="hHh lpR fff")
+  q-header(
+    elevated
+    class="bg-primary"
+  )
+    q-toolbar(
+      bordered
+      class="cinzel"
+      style="min-height:67px"
+    )
+      q-img(
+        src="../assets/logo.svg"
+        width="114px"
+        class="cursor-pointer"
+        @click="$router.push('/')"
+      )
+
+  q-page-container()
+    q-page(
+      class="row justify-center montserrat"
+      style="background-color: #F1F9FF"
+    )
+      router-view
+
+  q-footer(
+    class="bg-primary"
+  )
+
+</template>
