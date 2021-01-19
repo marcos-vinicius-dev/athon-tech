597ca10cc1d91f8f33930880ec8889ffd05c7d81
marcos vinicius <marcvinicius.1997@gmail.com>
2021-01-19 12:22:21 -0300

    feat: componente gerador de avatar

---
+++ src/components/molecules/avatars/AvatarGenerate.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'AvatarGenerate',
+
+  data () {
+    return {
+      src: null
+    }
+  },
+
+  mounted () {
+    axios
+      .get('/avatar')
+      .then(response => {
+        this.src = window.URL.createObjectURL(new Blob([response.data]))
+      })
+  }
+}
+</script>
+
+<template lang="pug">
+q-avatar(size="60px")
+  img(src="https://i.pravatar.cc")
+
+</div>
+
+</template>
